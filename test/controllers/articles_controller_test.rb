3a67b5abbdfc6709f8914df3a45b663060630246
chrislabarge <chrislabarge2@gmail.com>
2015-05-29 14:54:28 -0400

    created the update and destroy functionality along with flash messages

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,4 +4,17 @@ class ArticlesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+   def setup
+    @article = Article.new    
+    @article.title = "New Title"
+    @article.body = "This is the article Body"
+    @article.save
+   end
+
+   
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
+
+
 class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @article = Article.new    
+    @article.title = "New Title"
+    @article.body = "This is the article Body"
+    @article.save
+  end
+
+  test "article is saved" do
+  	#new_article = Article.new  	
+  	#new_article.title = "New Title"
+  	#new_article.body = "This is the article body"
+  	#new_article.save
+    assert @article != nil
+  	assert_not  @article.title.empty?
+  	assert_not  @article.body.empty?
+  end
+
+  test "new article should be valid" do 
+    assert @article.valid?
+  end
+
+  
+
+
 end
