705d099095d4f058ccde610bdf03a481a19091ca
chrislabarge <chrislabarge2@gmail.com>
2015-06-01 11:23:20 -0400

    making a comment system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,8 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+		@comment = Comment.new
+		@comment.article_id = @article.id #this is so we can create a new comment on a article page
 	end
 
 	def new
